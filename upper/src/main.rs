57b4a54ffaf27768976b426ef36979417cd989c9
ghatdev <issacs98@gmail.com>
2017-10-18 14:36:04 +0900

    upper completed

--- upper/src/main.rs
+++ upper/src/main.rs
@@ -1,3 +1,44 @@
+use std::fs;
+use std::env;
+use std::path::Path;
+
+fn upper_filename(file_name: &String, file_stem: &String) {
+    let uppered_stem = file_stem.to_uppercase();
+
+    let change_result = fs::rename(file_name, file_name.replace(file_stem,&uppered_stem));
+    
+    match change_result {
+        Ok(_) => return,
+        Err(e) => panic!(e),
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let arguments = env::args();
+
+    if arguments.len() < 2 {
+        println!("Usage: upper filename-to-upper");
+        return;
+    }
+
+    let todo = env::args().skip(1);
+    
+    for argument in todo {
+        let path = Path::new(&argument);
+
+        if !path.exists() {
+            println!("{} is not exists", path.display());
+            return;
+        }
+
+        if !path.is_file() {
+            println!("{} is not a file. Only file allowd", path.display());
+            return;
+        }
+
+        let stem = path.file_stem().unwrap().to_str().unwrap().to_string();
+
+        upper_filename(&argument, &stem);
+    }
+
 }
